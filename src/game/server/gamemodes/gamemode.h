ba124c33ea7ca9ae1b7d1c0889a8df2c898b57a8
Jason54 <33364110+Jason54@users.noreply.github.com>
2020-05-16 12:39:56 +0200

    Update gamemode.h

--- src/game/server/gamemodes/gamemode.h
+++ src/game/server/gamemodes/gamemode.h
@@ -3,6 +3,6 @@
 #ifndef GAME_SERVER_GAMEMODES_GAMEMODE_H
 #define GAME_SERVER_GAMEMODES_GAMEMODE_H
 
-#define GAME_NAME "DDraceNetwork"
-#define TEST_NAME "TestDDraceNetwork"
+#define GAME_NAME "DDrace"
+#define TEST_NAME "TestDDrace"
 #endif // GAME_SERVER_GAMEMODES_GAMEMODE_H
